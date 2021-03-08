7d7e75174d562b7e348af838126e837e031ea238
Miguel Najera <migueln1987x@gmail.com>
2021-03-08 06:51:29 -0600

    create functions to get price for gamestop and for any other ticker symbol

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import config
+import requests
+import json
 
 from twelvedata import TDClient
 
 API_KEY = config.api_key
 PREFIX = "https://api.twelvedata.com"
-ENDPOINT = "/quote"
+QUOTE_ENDPOINT = "/quote"
+PRICE_ENDPOINT = "/price"
 SYMBOL = "GME"
-INTERVAL = "1minapi_key"
+INTERVAL = "1min"
 EXCHANGE = "NYSE"
 TYPE = "Stock"
 FORMAT = "JSON"
@@ -14,9 +17,20 @@ FORMAT = "JSON"
 td = TDClient(apikey=API_KEY)
 
 
-def dummy_function():
-    print(API_KEY)
+def get_gme_price():
+    response = requests.get(f"{PREFIX}{PRICE_ENDPOINT}?symbol="
+                            f"{SYMBOL}&apikey={API_KEY}")
+    obj = json.loads(response.text)
+    return obj['price']
+
+
+def get_price(symbol):
+    response = requests.get(f"{PREFIX}{PRICE_ENDPOINT}?symbol="
+                            f"{symbol}&apikey={API_KEY}")
+    obj = json.loads(response.text)
+    return obj['price']
 
 
 if __name__ == '__main__':
-    dummy_function()
+    print(f"GameStop's price is: {get_gme_price()}")
+    print(f"Apple's price is: {get_price('AAPL')}")
