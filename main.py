fb171b811ae70459a8c198f9771f0ef372128df7
Miguel Najera <migueln1987x@gmail.com>
2021-03-08 05:43:35 -0600

    add requirements.txt and access api key from config

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import config
+
+from twelvedata import TDClient
+
+td = TDClient(apikey=config.api_key)
+
 
 def dummy_function():
-    print("Hello")
+    print(config.api_key)
 
 
 if __name__ == '__main__':
